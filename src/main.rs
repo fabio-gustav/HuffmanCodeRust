74a2f4c32caeca4d8839a0625203171338cafc92
auto <blaauwnicholas@gmail.com>
2024-11-25 17:50:21 -0600

    test commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+//node for huffman tree
+struct Node<'a> {
+    char: char,
+    freq: u32,
+    left_node: &'a Node<'a>,
+    right_node: &'a Node<'a>,
+}
+
+struct Tree<'a>{
+    head: &'a Node<'a>,
+}
+
+fn main(){
+    //example input data
+    let input_data: [char; 5] = ['a', 'b', 'c', 'd', 'e'];
 }
--- target/debug/deps/HuffmanCodeRust-60a1f984e268ae14.d
+++ target/debug/deps/HuffmanCodeRust-60a1f984e268ae14.d
@@ -1,5 +1,5 @@
-C:\Users\burke\Documents\GitHub\HuffmanCodeRust\target\debug\deps\libHuffmanCodeRust-60a1f984e268ae14.rmeta: src/main.rs
+C:\Users\Nick2\Desktop\Project\HuffmanCodeRust\target\debug\deps\libHuffmanCodeRust-60a1f984e268ae14.rmeta: src/main.rs
 
-C:\Users\burke\Documents\GitHub\HuffmanCodeRust\target\debug\deps\HuffmanCodeRust-60a1f984e268ae14.d: src/main.rs
+C:\Users\Nick2\Desktop\Project\HuffmanCodeRust\target\debug\deps\HuffmanCodeRust-60a1f984e268ae14.d: src/main.rs
 
 src/main.rs:
