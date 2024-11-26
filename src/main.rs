4e54d0541d355c99b988ec759abcfcb9ca00fd49
Nick <blaauwnicholas@gmail.com>
2024-11-26 11:01:36 -0600

    basic structure for the tree, need to figure out how to add null pointers in rust

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,39 @@
 struct Node<'a> {
     char: char,
     freq: u32,
-    left_node: &'a Node<'a>,
-    right_node: &'a Node<'a>,
+    left_node: &'a mut Node<'a>,
+    right_node: &'a mut Node<'a>,
 }
 
-struct Tree<'a>{
-    head: &'a Node<'a>,
+//min heap struct
+struct MinHeap {
+    //current size
+    size: u32,
+    
+    //capacity
+    capacity: u32,
 }
+
+//function that allocates a new node given its char and freq
+//returns a reference to that new node
+fn new_node(data: char, freq: u32) -> &'static Node<'static>{
+    &Node{
+        char:data,
+        freq: freq,
+        left_node: //todo
+        right_node: //todo
+    }
+}
+
+//creats a min heap for a given capacity
+//returns ownership to a minHeap
+fn create_min_heap(capacity: u32) -> MinHeap{
+    MinHeap{
+        size: 0,
+        capacity: capacity
+    }
+}
+
 
 fn main(){
     //example input data
