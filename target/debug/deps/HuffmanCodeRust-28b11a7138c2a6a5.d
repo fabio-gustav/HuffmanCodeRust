e1ac3a410eb028e6496df0d2034b61dd18b0259e
Shane Burke <67300048+fabio-gustav@users.noreply.github.com>
2024-11-25 18:15:28 -0600

    Delete target directory

--- target/debug/deps/HuffmanCodeRust-28b11a7138c2a6a5.d
+++
@@ -1,5 +0,0 @@
-C:\Users\burke\Documents\GitHub\HuffmanCodeRust\target\debug\deps\libHuffmanCodeRust-28b11a7138c2a6a5.rmeta: src/main.rs
-
-C:\Users\burke\Documents\GitHub\HuffmanCodeRust\target\debug\deps\HuffmanCodeRust-28b11a7138c2a6a5.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/HuffmanCodeRust.d
+++
@@ -1,5 +0,0 @@
-C:\Users\burke\Documents\GitHub\HuffmanCodeRust\target\debug\deps\HuffmanCodeRust.exe: src/main.rs
-
-C:\Users\burke\Documents\GitHub\HuffmanCodeRust\target\debug\deps\HuffmanCodeRust.d: src/main.rs
-
-src/main.rs:
