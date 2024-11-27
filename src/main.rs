a45a16e1e1c63206b41078fd794b84fdc99c299c
Nick <blaauwnicholas@gmail.com>
2024-11-26 18:31:07 -0600

    Finished structs for min heap and layed out most frameworks for the rest of the essential functions needed following the GFG guide. Note that not every function is included due to their functionality being able to be handled inside other functions.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 //node for huffman tree
-struct Node<'a> {
+struct Node <'a>{
+    //specific char
     char: char,
+
+    //frequency of the char
     freq: u32,
+
+    //reference to the left node
     left_node: &'a mut Node<'a>,
-    right_node: &'a mut Node<'a>,
+
+    //reference to the right node
+    right_node: &'a mut Node<'a>
 }
 
 //min heap struct
@@ -15,31 +22,53 @@ struct MinHeap<'a> {
     capacity: u32,
 
     //array of nodes in the tree
-        //need to find a valid format that works like the one here but doesnt give an error
-    nodes: [&'a mut Node<'a>; capacity]
+    nodes: Vec<&'a mut Node>
 }
 
-//function that allocates a new node given its char and freq
-//returns a reference to that new node
-fn new_node(data: char, freq: u32) -> &'static Node<'static>{
-    &Node{
-        char:data,
-        freq: freq,
-        left_node: None,
-        right_node: None,
-    }
+//function that allocates a new node given its char and freq and adds it to the heap
+fn add_new_node(data: char, freq: u32, tree: &'a mut MinHeap<'a>){
+    //todo
 }
 
-//creats a min heap for a given capacity
+//creates a min heap for a given capacity
 //returns ownership to a minHeap
-fn create_min_heap(capacity: u32) -> MinHeap{
-    MinHeap{
-        size: 0,
-        capacity: capacity,
-        
-    }
+fn create_min_heap(capacity: u32) -> MinHeap<'a>{
+    //todo
 }
 
+//swaps two min heap nodes
+fn swap_min_heap_node(node1: &'a mut Node<'a>, node2: &'a mut Node<'a>){
+    //todo
+}
+
+//heapify the tree
+fn min_heapify(tree: &'a mut MinHeap<'a>, index: u32){
+    //todo
+}
+
+//extracts minimum value node from heap
+fn extract_min(heap: &'a mut MinHeap<'a>) -> &'a mut Node<'a> {
+    //todo
+}
+
+//builds the min heap
+fn build_min_heap(heap: &'a mut MinHeap<'a>){
+    //todo
+}
+
+//creats a min heap of capacity equal to size
+//inserts all chars of data[] in min heap
+fn create_and_build_min_heap(data: Vec<char>, freq: Vec<u32>, size: u32)->&'a mut MinHeap<'a>{
+    //todo
+}
+
+//builds huffman tree
+//todo
+fn build_huffman_tree()
+
+//builds and prints huffman tree
+//todo
+fn HuffmanCodes()
 
 fn main(){
     //example input data
