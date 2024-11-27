8a6b3201925c874054e7169dd9a2a0db2fd99d29
Nick <blaauwnicholas@gmail.com>
2024-11-27 09:40:29 -0600

    completed heapify and create_min_heap functions based on GFG's min heap functions

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,8 @@ struct Node {
 
 //min heap struct
 struct MinHeap {
-    //current size
-    size: u32,
-    
-    //capacity
-    capacity: u32,
+    //current size of the heap
+    size: usize,
 
     //array of nodes in the tree
     //root will be at nodes[0]
@@ -22,8 +19,7 @@ struct MinHeap {
         //arr[(2*i)+1] = left child node
         //arr[(2*i)+2] = right child node
     nodes: Vec<Node>
-}
-
+    }
 
 impl MinHeap{
     //adds a node to the unsorted node vector with char its freq
@@ -39,22 +35,50 @@ impl MinHeap{
         self.nodes.push(node_to_add)
     }
 
-    //sorts all of the nodes in the array so that they represent a min heap
-    //after this is done, self.nodes will be a min heap
-    fn heapify(&mut self){
-        //todo
+    //heapifys a single node
+    //based off of GFG heapify function
+    fn heapify(&mut self, index: usize){
+        //Indices of relevant roots
+        let mut smallest: usize = index;
+        let left: usize = 2 * index + 1;
+        let right: usize = 2 * index + 2;
+        let heap_size: usize = self.size;
+
+        //if left child is smaller than root
+        if left < heap_size && self.nodes[left].freq < self.nodes[smallest].freq{
+            smallest = left;
+        }
+
+        //if right child is smaller than smallest so far
+        if right < heap_size && self.nodes[right].freq < self.nodes[smallest].freq{
+            smallest = right;
+        }
+
+        //if smallest is not root
+        if smallest != index{
+            self.nodes.swap(index, smallest);
+            self.heapify(smallest)
+        }
+        }
+
+        //creates a minimum heap out of unsorted node vector
+        //based off of GFG buildHeap function
+        fn create_min_heap(&mut self){
+            //start at index of last non-leaf
+            let n: usize = self.size;
+
+            //heapify each node, starting at the last non leaf node
+            //start at index (n-1)/2
+            for i in (0..((n-1)/2)+1).rev(){
+                self.heapify(i);
+            }
+        }
+
     }
-
-}
-
-//creates a minimum heap out of unsorted node vector
-fn create_min_heap(heap: &mut MinHeap){
-    //todo
-}
 
 //builds the huffman tree out of the passed in min heap
 fn build_huffman_tree(heap: &mut MinHeap){
-    //todo
+    
 }
 
 
